73f1a85b9ba3412aa399096579469d2b0ce6f5ce
darth.morien <darth.morien@gmail.com>
2018-05-21 00:08:11 +0200

    Percolation class - no errors, but does nothing

--- src/Percolation.java
+++ src/Percolation.java
@@ -2,6 +2,8 @@ import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
+import java.util.Random;
+import java.util.Scanner;
 import java.util.logging.Logger;
 
 public class Percolation {
@@ -10,15 +12,22 @@ public class Percolation {
 	private int n;
 	private int uniqueCount = 1;
 	WeightedQuickUnionUF alg = new WeightedQuickUnionUF(n * n);
-
+	private int  row;
+	private int  col; 
+	
 	private final static Logger LOGGER = Logger.getLogger(Percolation.class.getName());
 
 	// create n-by-n grid, with all sites blocked
 	public Percolation(int n) {
+		this.n = n;
 		if (n <= 0) {
 			throw new java.lang.IllegalArgumentException();
 		} else {
-			grid = new int[n][n];// initialized with 0, that means "blocked"
+			this.grid = new int[n][n];// initialized with 0, that means "blocked"
+			this.row = StdRandom.uniform(1,n);
+			this.col = StdRandom.uniform(1,n);
+			LOGGER.info("Row is " + row + ", " + " col is " + col);
+			
 		}
 	}
 
@@ -28,7 +37,7 @@ public class Percolation {
 			throw new java.lang.IllegalArgumentException();
 		} else {
 			if (!isOpen(row, col)) {
-				grid[row + 1][col + 1] = uniqueCount;
+				grid[row - 1][col - 1] = uniqueCount;
 				uniqueCount++;
 				LOGGER.info("The site on " + row + ", " + col + " is now open with value " + uniqueCount);
 			} else {
@@ -42,9 +51,9 @@ public class Percolation {
 	// 0 means closed; else means open
 	public boolean isOpen(int row, int col) {
 		if ((row > n + 1 || row < 1) || (col > n + 1 || col < 1)) {
-			throw new java.lang.IllegalArgumentException();
+			throw new java.lang.IllegalArgumentException("row is "+ row + " col is "+ col);
 		} else {
-			if (grid[row + 1][col + 1] > 0) {
+			if (grid[row - 1][col - 1] > 0) {
 				return true;
 			} else
 				return false;
@@ -57,23 +66,26 @@ public class Percolation {
 			throw new java.lang.IllegalArgumentException();
 		}
 
-		for (int i = 1; i <= n; i++) {
-			if (alg.connected(grid[row][col], grid[1][i])) {
-				return true;
+		
+			for (int i = 0; i <= n; i++) {
+				//if (isOpen(row, col)) {
+					if (isOpen(row, col) && alg.connected(grid[row-1][col-1], grid[0][i])) {
+						return true;
+				//}
 			}
 		}
+		
 		return false;
 	}
 
 	// number of open sites
 	public int numberOfOpenSites() {
-		return 0;
+		return alg.count();
 	}
 
 	// does the system percolate?
 	public boolean percolates() {
-		processAll();
-		
+			
 		for (int i = 1; i <= n; i++) {
 			if (isFull(n,i)) {
 				return true;
@@ -81,24 +93,6 @@ public class Percolation {
 		}
 		return false;
 	}
-
-	/*private void firstRow() {
-
-		for (int col = 1; col <= n; col++) {
-			if (isOpen(1, col)) {
-
-				if ((isOpen(1, col + 1)) && (col + 1 <= n)) {
-					alg.union(grid[1][col], grid[1][col + 1]);
-				}
-
-				if (isOpen(2, col)) {
-					alg.union(grid[1][col], grid[2][col]);
-				}
-
-			}
-		}
-
-	}*/
 
 	private void processAll() {
 
@@ -118,9 +112,25 @@ public class Percolation {
 		}
 
 	}
+	
+	private void monteCarlo(){
+			do {
+				if (!isOpen(row, col)) {
+					open(row, col);
+					processAll();
+				}
+				
+			} while (!percolates());
+		
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Enter the grid size n");
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		Percolation perc= new Percolation(n);
+		perc.monteCarlo();
+		System.out.println("System percolated with " + perc.numberOfOpenSites() +" open sites");
 
 	}
 
